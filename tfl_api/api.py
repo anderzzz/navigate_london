20598d25851866898ec17d59714f41bb6df01647
anderzzz <ohrn.anders@gmail.com>
2024-10-25 13:26:21 +0200

    checkpoint commit while rewriting the journey planner parameters model

--- tfl_api/api.py
+++ tfl_api/api.py
@@ -3,11 +3,13 @@
 """
 from typing import Sequence, Optional
 from enum import Enum
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, field_validator
+from datetime import datetime
 
 from tfl_api import TFLClient
 
-class Modes(Enum):
+class Modes(str, Enum):
+    """The modes of transportation."""
     BUS = 'bus'
     TUBE = 'tube'
     RAIL = 'rail'
@@ -16,18 +18,21 @@ class Modes(Enum):
 
 
 class WalkingSpeed(str, Enum):
+    """Speeds of walking."""
     SLOW = "slow"
     AVERAGE = "average"
     FAST = "fast"
 
 
 class JourneyPreference(str, Enum):
+    """Preferences on what to optimize in journey planning."""
     LEAST_INTERCHANGE = "least_interchange"
     LEAST_WALKING = "least_walking"
     LEAST_TIME = "least_time"
 
 
 class TimeIs(str, Enum):
+    """Specification on what a time of day denotes."""
     DEPARTING = "departing"
     ARRIVING = "arriving"
 
@@ -38,7 +43,7 @@ class JourneyModesAvailable:
         self._endpoint = 'Journey/Meta/Modes'
 
     def __call__(self):
-        """Retrieve all available transportation modes that the journey planner API deals with
+        """Retrieve all available transportation modes that the journey planner API deals with.
 
         """
         return self.client.get(self._endpoint)
@@ -64,6 +69,14 @@ class JourneyPlannerSearchParams(BaseModel):
     walking_optimization: Optional[bool] = None
     taxi_only: Optional[bool] = None
 
+    @field_validator('date')
+    @classmethod
+    def validate_date(cls, d):
+        try:
+            datetime.strptime(d, '%Y%m%d')
+        except ValueError:
+
+
 
 class JourneyPlannerSearch:
     def __init__(self, client: TFLClient):
@@ -74,26 +87,7 @@ class JourneyPlannerSearch:
                  from_loc: str,
                  to_loc: str,
                  **params):
-        """
-                 via: Optional[str] = None,
-                 national_search: Optional[bool] = None,
-                 date: Optional[str] = None,
-                 time: Optional[str] = None,
-                 time_is: Optional[str] = None,
-                 journey_preference: Optional[str] = None,
-                 mode: Optional[str] = None,
-                 accessibility_preference: Optional[str] = None,
-                 max_transfer_minutes: Optional[int] = None,
-                 max_walking_minutes: Optional[int] = None,
-                 walking_speed: Optional[str] = None,
-                 cycle_preference: Optional[str] = None,
-                 adjustment: Optional[str] = None,
-                 bike_proficiency: Optional[str] = None,
-                 alternative_cycle: Optional[bool] = None,
-                 alternative_walking: Optional[bool] = None,
-                 walking_optimization: Optional[bool] = None,
-                 taxi_only: Optional[bool] = None,"""
-        """Plan a journey between two locations.
+        """Plan a journey between two locations, given a set of preferences and times.
         
         """
         _url = f'{self._endpoint}/{from_loc}/to/{to_loc}'
