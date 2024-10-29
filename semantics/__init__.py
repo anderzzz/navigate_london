6047de23afd7427def484de8288a1cf6e6fc2f02
anderzzz <ohrn.anders@gmail.com>
2024-10-29 15:16:37 +0100

    Add message stack and preparations for tools

---
+++ semantics/__init__.py
@@ -0,0 +1,4 @@
+from .anthropic.engine import (
+    Engine,
+    AnthropicMessageParams,
+)
--- semantics/anthropic/engine.py
+++ semantics/anthropic/engine.py
@@ -2,9 +2,34 @@
 
 """
 import os
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List, Literal
+from dataclasses import dataclass
+from pydantic import BaseModel, Field
 
 from anthropic import Anthropic
+
+
+@dataclass
+class AnthropicMessageParams:
+    model: str
+    max_tokens: int
+    temperature: float = 0.7
+
+
+class Message(BaseModel):
+    role: Literal['user', 'tool', 'assistant']
+    content: str
+    tool: Optional[str] = None
+
+
+class MessageStack(BaseModel):
+    messages: List[Message] = Field(default_factory=list)
+
+    def add_message(self, role: str, content: str, tool: Optional[str] = None):
+        self.messages.append(Message(role=role, content=content, tool=tool))
+
+    def to_list_dicts(self):
+        return [message.model_dump() for message in self.messages]
 
 
 class Engine:
@@ -14,20 +39,37 @@ class Engine:
     def __init__(self,
                  api_key_env_var: str,
                  system_prompt: str,
-                 tools: Optional[Dict[str, Any]] = None
+                 message_params: AnthropicMessageParams,
+                 tools: Optional[List[Dict[str, Any]]] = None,
                  ):
         api_key = os.getenv(api_key_env_var)
         if not api_key:
             raise ValueError(f'Did not find an API key in environment variable {api_key_env_var}')
         self.client = Anthropic(api_key=os.getenv(api_key_env_var))
         self.system_prompt = system_prompt
+        self.message_params = message_params
+        if tools is None:
+            tools = []
+        self.tools = tools
+
+        self._message_stack = MessageStack()
 
     def process(self,
                 input_prompt: str,
-                use_tool: Optional[str] = None,
+                tool_choice: str = 'auto',
                 respond_to_tool: Optional[bool] = False,
                 ):
         """Bla bla
 
         """
-        pass
+        self._message_stack.add_message(
+            role='user',
+            content=input_prompt,
+        )
+        response = self.client.messages.create(
+            system=self.system_prompt,
+            model=self.message_params.model,
+            max_tokens=self.message_params.max_tokens,
+            temperature=self.message_params.temperature,
+            tool_choice=tool_choice,
+        )
